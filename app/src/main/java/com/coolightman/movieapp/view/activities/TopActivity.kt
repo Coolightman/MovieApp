5101a2cf3950299837c619f628ab58499993ac6d
Coolightman <drugak48@gmail.com>
2021-10-23 23:20:21 +0300

    add topType setter

--- app/src/main/java/com/coolightman/movieapp/view/activities/TopActivity.kt
+++ app/src/main/java/com/coolightman/movieapp/view/activities/TopActivity.kt
@@ -2,12 +2,15 @@ package com.coolightman.movieapp.view.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.coolightman.movieapp.R
-import com.coolightman.movieapp.model.data.Favorite
 import com.coolightman.movieapp.model.data.Movie
+import com.coolightman.movieapp.model.enums.Top
 import com.coolightman.movieapp.view.adapters.TopAdapter
 import com.coolightman.movieapp.viewmodel.TopViewModel
 import kotlinx.android.synthetic.main.activity_top.*
@@ -66,6 +69,33 @@ class TopActivity : AppCompatActivity() {
     }
 
     private fun listeners() {
+        spinnerListener()
+    }
 
+    private fun spinnerListener() {
+        spinnerTops.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                setCurrentTopType(position)
+                layoutManager.scrollToPosition(0)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Log.i("spinnerTops", "NothingSelected")
+            }
+        }
+    }
+
+    private fun setCurrentTopType(position: Int) {
+        val topType = when (position) {
+            0 -> Top.TOP_100_POPULAR_FILMS
+            1-> Top.TOP_250_BEST_FILMS
+            else -> Top.TOP_AWAIT_FILMS
+        }
+        topViewModel.setTopType(topType)
     }
 }
--- app/src/main/java/com/coolightman/movieapp/viewmodel/TopViewModel.kt
+++ app/src/main/java/com/coolightman/movieapp/viewmodel/TopViewModel.kt
@@ -2,10 +2,19 @@ package com.coolightman.movieapp.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.coolightman.movieapp.model.enums.Top
 import com.coolightman.movieapp.model.repository.MovieRepository
 
 class TopViewModel(application: Application) : AndroidViewModel(application) {
     private val movieRepository = MovieRepository(application)
 
+    private var topType = Top.TOP_100_POPULAR_FILMS
+    private var topTypeTrigger = MutableLiveData<Top>()
+
+    fun setTopType(topType: Top) {
+        this.topType = topType
+        this.topTypeTrigger.value = topType
+    }
 
 }
